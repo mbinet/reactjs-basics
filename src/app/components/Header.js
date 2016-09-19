5b46ece5099a212f7ff85fb02f91c3f2ab682d2a
mbinet <mbinet@student.42.fr>
2016-09-19 17:33:58 +0200

    first commit

---
+++ src/app/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+export const Header = (props) => {
+    return (
+        <div className="navbar navbar-default">
+            <div className="container">
+                <div className="navbar-header">
+                    <ul className="nav navbar-nav">
+                        <li><a href="#">{props.homeLink}</a></li>
+                    </ul>
+                </div>
+            </div>
+        </div>
+    );
+};
